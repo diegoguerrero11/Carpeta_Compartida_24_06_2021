3c495bea81c0039304cf9f5c9b2281f7602e6728
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-06-24 21:24:08 -0500

    Carpeta Compartida 24-06-2021

---
+++ profesor/main.js
@@ -0,0 +1,6 @@
+addEventListener("DOMContentLoaded", function(){
+    
+    let img = document.createElement("IMG");
+    img.src = "https://creantelab.co/wp-content/uploads/2020/09/javascript-illustration.png"
+    document.body.appendChild(img);
+})
